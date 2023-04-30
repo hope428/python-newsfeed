e2bdcd4918a8ddc68c687a5b4bf98211d92eb0d8
Leon Forsythe <lvforsythe95@gmail.com>
2023-04-30 18:00:48 -0400

    added database queries for getting post information for home page. also added close db functions

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,7 +1,9 @@
 from datetime import datetime
-from db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from app.db import Base
+from .Vote import Vote
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
+from sqlalchemy.orm import relationship, column_property
+
 
 class Post(Base):
     __tablename__ = 'posts'
@@ -11,7 +13,11 @@ class Post(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-    
+    # vote_count = column_property(
+    #     select(Vote.id).where(Vote.post_id == id)
+    # )
+
+    votes = relationship('Vote', cascade='all,delete')
+
     user = relationship('User')
     comments = relationship('Comment', cascade='all,delete')
-
