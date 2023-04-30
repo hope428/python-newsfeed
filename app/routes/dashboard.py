98b037e4af521bb55c1b35a9e4c488ab53064ca1
Leon Forsythe <lvforsythe95@gmail.com>
2023-04-30 18:47:42 -0400

    added db query to dashboard routes for editing individual post by id

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from app.models import Post
+from app.db import get_db
 
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
@@ -9,5 +11,7 @@ def dash():
 
 @bp.route('/edit/<id>')
 def edit(id):
-    return render_template('edit-post.html')
+    db = get_db()
+    currentPost = currentPost = db.query(Post).filter(Post.id == id).one()
+    return render_template('edit-post.html', post=currentPost)
 
