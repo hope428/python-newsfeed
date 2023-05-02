347f6b92cf5fc0c549656199dc7820b17ff909e2
Leon Forsythe <lvforsythe95@gmail.com>
2023-05-01 20:51:50 -0400

    added sessions to keep logged in user info"

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,21 +1,34 @@
-from flask import Blueprint, request, jsonify
+import sys
+from flask import Blueprint, request, jsonify, session
 from app.models import User
 from app.db import get_db
 
 bp = Blueprint('api', __name__, url_prefix='/api')
+
 
 @bp.route('/users', methods=['POST'])
 def signup():
     data = request.get_json()
     db = get_db()
 
-    newUser = User(
-        username = data['username'],
-        email = data['email'],
-        password = data['password']
-    )
+    try:
+        newUser = User(
+            username=data['username'],
+            email=data['email'],
+            password=data['password']
+        )
 
-    db.add(newUser)
-    db.commit()
+        db.add(newUser)
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+        db.rollback()
+        return jsonify(message='Signup failed'), 500
 
-    return jsonify(id = newUser.id)
+    session.clear()
+    session['user_id'] = newUser.id
+    session['loggedIn'] = True
+    return jsonify(id=newUser.id)
+
+# @bp.route('/users/login', methods=['POST'])
+# def login():
--- app/routes/home.py
+++ app/routes/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect
 from app.models import Post
 from app.db import get_db
 
@@ -8,14 +8,16 @@ bp = Blueprint('home', __name__, url_prefix='/')
 def index():
     db = get_db()
     postArr = db.query(Post).order_by(Post.created_at.desc()).all()
-    return render_template('homepage.html', posts=postArr)
+    return render_template('homepage.html', posts=postArr, loggedIn=session.get('loggedIn'))
 
 @bp.route('/login')
 def login():
-    return render_template('login.html')
+    if session.get('loggedIn') is None:
+        return render_template('login.html')
+    return redirect('/dashboard')
 
 @bp.route('/post/<id>')
 def single(id):
     db = get_db()
     currentPost = db.query(Post).filter(Post.id == id).one()
-    return render_template('single-post.html', post=currentPost)
+    return render_template('single-post.html', post=currentPost, loggedIn=session.get('loggedIn'))
