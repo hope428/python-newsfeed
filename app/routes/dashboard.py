91b35104e274c498936ec37d6ef93f1742e9a17d
Leon Forsythe <lvforsythe95@gmail.com>
2023-05-02 20:04:57 -0400

    added login_required middleware to dashboard and edit routes

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template, session
 from app.models import Post
 from app.db import get_db
+from app.utils.auth import login_required
 
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 @bp.route('/')
+@login_required
 def dash():
     db = get_db()
     postArr = db.query(Post).filter(Post.user_id == session.get('user_id')).all()
@@ -12,6 +14,7 @@ def dash():
 
 
 @bp.route('/edit/<id>')
+@login_required
 def edit(id):
     db = get_db()
     currentPost = currentPost = db.query(Post).filter(Post.id == id).one()
